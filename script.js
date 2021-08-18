842cc699b22cae19eed37c9fe4182197a89df91c
tinahaq <87503827+tinahaq@users.noreply.github.com>
2021-08-18 10:00:48 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("navbar").style.display = "block";
-  }
-  
-  /* Close/hide the sidenav */
-  function closeNav() {
-    document.getElementById("navbar").style.display = "none";
-  }
