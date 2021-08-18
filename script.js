94ad3d2b9aa67992e2dbd66d1e47af1acbf2e7c8
tinahaq <tinnyhaq@gmail.com>
2021-08-18 09:58:13 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("navbar").style.display = "block";
+  }
+  
+  /* Close/hide the sidenav */
+  function closeNav() {
+    document.getElementById("navbar").style.display = "none";
+  }
